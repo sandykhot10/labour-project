c2bbc5f09f240ea2ad82b6c1a77ab1de099d45bc
sandykhot10 <sandykhot10@gmail.com>
2024-10-07 21:20:30 +0530

    decreasing spaces

--- application.php
+++ application.php
@@ -1,18 +1,13 @@
 <?php
 session_start();
 ?>
-
-
 <!DOCTYPE html>
 <html>
-
-
 <!-- Mirrored from preview.byaviators.com/template/profession/registration.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 19 Aug 2018 11:09:11 GMT -->
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" type="text/css">
     <link href="assets/fonts/profession/style.css" rel="stylesheet" type="text/css">
     <link href="assets/libraries/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -20,15 +15,10 @@ session_start();
     <link href="assets/libraries/bootstrap-select/css/bootstrap-select.min.css" rel="stylesheet" type="text/css">
     <link href="assets/libraries/bootstrap-wysiwyg/bootstrap-wysiwyg.min.css" rel="stylesheet" type="text/css">
     <link href="assets/css/profession-black-green.css" rel="stylesheet" type="text/css" id="style-primary">
-
     <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.png">
-
     <title>Profession - Account Registration</title>
 </head>
-
-
 <body class="hero-content-dark footer-dark">
-
 <div class="page-wrapper">
     <div class="header-wrapper">
     <div class="header">
@@ -41,26 +31,19 @@ session_start();
                             <span class="header-logo-text"><span class="header-logo-highlight">.</span>SeekForJob</span>
                         </a>
                     </div><!-- /.header-logo-->
-
                     <div class="header-slogan">
-
                     </div><!-- /.header-slogan-->
                 </div><!-- /.header-brand -->
-
                 <ul class="header-actions nav nav-pills">
                     <li><a href="login.php"><i class="fa fa-user"></i>&nbsp;
-
                         <?php
-
                         if(isset($_SESSION['username'])){
                             echo $_SESSION['username'];
                         }
                         else{
                          echo "Login";
                         }
-                        
                         ?>
-
                     </a></li>
                     <li><a href="registration.php">Sign Up</a></li>
                     <li><a href="create-resume.php" class="primary">Create Resume</a></li>
@@ -70,7 +53,6 @@ session_start();
                     }
                     ?>
                 </ul><!-- /.header-actions -->
-
                 <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".header-nav">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
@@ -79,24 +61,20 @@ session_start();
                 </button>
             </div><!-- /.container -->
         </div><!-- /.header-top -->
-
         <div class="header-bottom">
             <div class="container">
                 <ul class="header-nav nav nav-pills collapse">
                     <li >
                         <a href="index-2.php">Home</a>
                     </li>
-
                     <li >
                         <a href="#">Companies <i class="fa fa-chevron-down"></i></a>
-
                         <ul class="sub-menu">
                             <li><a href="companies.php">Company Listing</a></li>
                             <li><a href="company-detail.php">Company Detail</a></li>
                             <li><a href="job_vacancy.php">Create Job Vacancy</a></li>
                         </ul>
                     </li>
-
                     <li >
                         <a href="#">Positions <i class="fa fa-chevron-down"></i> </a>
                         <ul class="sub-menu">
@@ -104,39 +82,30 @@ session_start();
                             <li><a href="position-detail.php">Position Detail</a></li>
                         </ul>
                     </li>
-
                     <li >
                         <a href="#">Candidates <i class="fa fa-chevron-down"></i></a>
-
                         <ul class="sub-menu">
                             <li><a href="candidates.php">Candidates List</a></li>
                             <li><a href="resume.php">Resume</a></li>
                             <li><a href="create-resume.php">Create Resume</a></li>
                         </ul><!-- /.sub-menu -->
                     </li>
-
                     <li class="active">
                         <a href="#">Pages <i class="fa fa-chevron-down"></i></a>
-
                         <ul class="sub-menu">
-
                             <li><a href="login.php">Login</a></li>
                             <li><a href="registration.php">Registration</a></li>
                         </ul>
                     </li>
                 </ul>
-
                 <div class=" hidden-sm">
                     <form method="get" action="signupsuccess.php">
-
                     </form>
                 </div><!-- /.header-search -->
             </div><!-- /.container -->
         </div><!-- /.header-bottom -->
     </div><!-- /.header -->
 </div><!-- /.header-wrapper-->
-
-
     <div class="main-wrapper">
         <div class="main">
             <div class="document-title">
@@ -144,9 +113,7 @@ session_start();
                     <h1 class="center">Create Application</h1>
                 </div><!-- /.container -->
             </div><!-- /.document-title -->
-
             <div class="col-sm-12">
-
             <form id="application-form" action="application_success.php" method="POST">
                 <div class="row">
                         <div class="col-sm-6">
@@ -154,24 +121,20 @@ session_start();
                                 <label for="form-register-username">First Name</label>
                                 <input type="text" class="form-control" id="form-applicant-firstname" name="form-applicant-firstname"><br>
                             </div><!-- /.form-group -->
-
                             <div class="form-group">
                                 <label for="form-register-username">Last Name</label>
                                 <input type="text" class="form-control" id="form-applicant-lastname" name="form-applicant-lastname"><br>
                             </div><!-- /.form-group -->
-
                             <div class="form-group">
                                 <label for="form-register-username">Post</label>
                                 <input type="text" class="form-control" id="form-applicant-post" name="form-applicant-post"><br>
                             </div><!-- /.form-group -->
                         </div><!-- /.col-* -->
-
                         <div class="col-sm-6">
                             <div class="form-group">
                                 <label for="form-register-username">City</label>
                                 <input type="text" class="form-control" id="form-applicant-city" name="form-applicant-city"><br>
                             </div><!-- /.form-group -->
-
                             <div class="form-group">
                                 <label for="form-register-username">State</label>
                                 <input type="text" class="form-control" id="form-applicant-state" name="form-applicant-state"><br>
@@ -192,68 +155,52 @@ session_start();
                 </div>
             </form>
         </div><!-- /.col -->
-
-
-    </div><!-- /.main -->
+    </div><!-- /.main ->
     </div><!-- /.main-wrapper -->
 </div><!-- /.page-wrapper -->
-
 <div class="action-bar">
     <div class="action-bar-content">
         <div class="action-bar-chapter">
             <strong>Choose Page Layout</strong>
-
             <ul>
                 <li><a href="#" data-action="layout-wide" class="active">Wide</a></li><li><a href="#" data-action="layout-boxed">Boxed</a></li>
             </ul>
         </div><!-- /.action-bar-chapter -->
-
         <div class="action-bar-chapter">
             <strong>Header Style</strong>
-
             <ul>
                 <li><a href="#" class="active" data-action="header-light">Light</a></li><li><a href="#" data-action="header-dark">Dark</a></li>
             </ul>
         </div><!-- /.action-bar-chapter -->
-
         <div class="action-bar-chapter">
             <strong>Navigation Style</strong>
-
             <ul>
                 <li><a href="#" data-action="navigation-light">Light</a></li><li><a href="#" class="active" data-action="navigation-dark">Dark</a></li>
             </ul>
         </div><!-- /.action-bar-chapter -->
-
         <div class="action-bar-chapter">
             <strong>Hero Content Style</strong>
-
             <ul>
                 <li><a href="#" data-action="hero-content-light">Light</a></li><li><a href="#" class="active" data-action="hero-content-dark">Dark</a></li>
             </ul>
         </div><!-- /.action-bar-chapter -->
-
         <div class="action-bar-chapter">
             <strong>Footer Style</strong>
-
             <ul>
                 <li><a href="#" data-action="footer-light">Light</a></li><li><a href="#"  class="active" data-action="footer-dark">Dark</a></li>
             </ul>
         </div><!-- /.action-bar-chapter -->
-
         <div class="action-bar-chapter">
             <strong>Color Combination</strong>
-
             <table>
                 <tr>
                     <td><a href="assets/css/profession-purple-red.css">Purple / Red</a></td>
                     <td><a href="assets/css/profession-blue-cyan.css">Blue / Cyan</a></td>
                 </tr>
-
                 <tr>
                     <td><a href="assets/css/profession-gray-orange.css">Gray / Orange</a></td>
                     <td><a href="assets/css/profession-black-green.css" class="active">Black / Green</a></td>
                 </tr>
-
                 <tr>
                     <td><a href="assets/css/profession-blue-navy.css">Blue / Navy</a></td>
                     <td></td>
@@ -261,14 +208,9 @@ session_start();
             </table>
         </div><!-- /.action-bar-chapter -->
     </div><!-- /.action-bar-content -->
-
-
 </div><!-- /.action-bar -->
-
-
 <script type="text/javascript" src="assets/js/jquery.js"></script>
 <script type="text/javascript" src="assets/js/jquery.ezmark.js"></script>
-
 <script type="text/javascript" src="assets/libraries/bootstrap-sass/javascripts/bootstrap/collapse.js"></script>
 <script type="text/javascript" src="assets/libraries/bootstrap-sass/javascripts/bootstrap/dropdown.js"></script>
 <script type="text/javascript" src="assets/libraries/bootstrap-sass/javascripts/bootstrap/tab.js"></script>
@@ -276,16 +218,10 @@ session_start();
 <script type="text/javascript" src="assets/libraries/bootstrap-fileinput/js/fileinput.min.js"></script>
 <script type="text/javascript" src="assets/libraries/bootstrap-select/js/bootstrap-select.min.js"></script>
 <script type="text/javascript" src="assets/libraries/bootstrap-wysiwyg/bootstrap-wysiwyg.min.js"></script>
-
 <script type="text/javascript" src="assets/libraries/cycle2/jquery.cycle2.min.js"></script>
 <script type="text/javascript" src="assets/libraries/cycle2/jquery.cycle2.carousel.min.js"></script>
-
 <script type="text/javascript" src="assets/libraries/countup/countup.min.js"></script>
-
 <script type="text/javascript" src="assets/js/profession.js"></script>
 <script type="text/javascript" src="assets/js/myJavaScript.js"></script>
-
-
 </body>
-
 </html>
